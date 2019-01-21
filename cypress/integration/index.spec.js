21bbd3c49ea1f0da2d1cb2e9ffc777f492727e44
james <james@netlag.xyz>
2019-01-21 17:24:21 -0500

    Added Cypress tests

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,5 +1,12 @@
 describe('Index', () => {
-    it('users should be able to view the "/" page', () => {
-        cy.visit('/').get('h1').contains('All Users');
+    it('should display the page correctly if a user is not logged in', () => {
+        cy
+            .visit('/')
+            .get('h1').contains('All Users')
+            .get('.navbar-burger').click()
+            .get('.navbar-item').contains('User Status').should('not.be.visible')
+            .get('.navbar-item').contains('Log Out').should('not.be.visible')
+            .get('.navbar-item').contains('Log In')
+            .get('.navbar-item').contains('Register');
     });
 });
