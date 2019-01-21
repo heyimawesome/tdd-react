939273688f9574f820761abf9b2dc34ec815ba76
james <james@netlag.xyz>
2019-01-21 18:39:01 -0500

    Refactored Form component so it'll manage it's own state

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -14,22 +14,11 @@ class App extends Component {
     super();
     this.state = {
       users: [],
-      username: '',
-      email: '',
       title: 'TestDriven.io',
-      formData: {
-        username: '',
-        email: '',
-        password: ''
-      },
       isAuthenticated: false
     };
 
-    this.addUser = this.addUser.bind(this);
-    this.clearFormState = this.clearFormState.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
-    this.handleFormChange = this.handleFormChange.bind(this);
+    this.loginUser = this.loginUser.bind(this);
     this.logoutUser = this.logoutUser.bind(this);
   }
 
@@ -54,68 +43,10 @@ class App extends Component {
       });
   }
 
-  addUser(event) {
-    event.preventDefault();
-
-    const data = {
-      username: this.state.username,
-      email: this.state.email
-    };
-
-    axios
-      .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
-      .then(res => {
-        this.getUsers();
-        this.setState({ username: '', email: '' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
-  handleChange(event) {
-    const obj = {};
-    obj[event.target.name] = event.target.value;
-    this.setState(obj);
-  }
-
-  handleUserFormSubmit(event) {
-    event.preventDefault();
-    const formType = window.location.href.split('/').reverse()[0];
-    let data = {
-      email: this.state.formData.email,
-      password: this.state.formData.password
-    };
-    if (formType === 'register') {
-      data.username = this.state.formData.username;
-    }
-    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`;
-    console.log(data);
-    axios
-      .post(url, data)
-      .then(res => {
-        this.clearFormState();
-        window.localStorage.setItem('authToken', res.data.auth_token);
-        this.setState({ isAuthenticated: true });
-        this.getUsers();
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
-  handleFormChange(event) {
-    const obj = this.state.formData;
-    obj[event.target.name] = event.target.value;
-    this.setState(obj);
-  }
-
-  clearFormState() {
-    this.setState({
-      formData: { username: '', email: '', password: '' },
-      username: '',
-      email: ''
-    });
+  loginUser(token) {
+    window.localStorage.setItem('authToken', token);
+    this.setState({ isAuthenticated: true });
+    this.getUsers();
   }
 
   logoutUser() {
@@ -145,16 +76,15 @@ class App extends Component {
                       </div>
                     )}
                   />
+                  <Route exact path='/about' component={About} />
                   <Route
                     exact
                     path='/register'
                     render={() => (
                       <Form
                         formType={'Register'}
-                        formData={this.state.formData}
-                        handleUserFormSubmit={this.handleUserFormSubmit}
-                        handleFormChange={this.handleFormChange}
                         isAuthenticated={this.state.isAuthenticated}
+                        loginUser={this.loginUser}
                       />
                     )}
                   />
@@ -163,11 +93,9 @@ class App extends Component {
                     path='/login'
                     render={() => (
                       <Form
-                        formType={'Log In'}
-                        formData={this.state.formData}
-                        handleUserFormSubmit={this.handleUserFormSubmit}
-                        handleFormChange={this.handleFormChange}
+                        formType={'Login'}
                         isAuthenticated={this.state.isAuthenticated}
+                        loginUser={this.loginUser}
                       />
                     )}
                   />
@@ -181,7 +109,6 @@ class App extends Component {
                       />
                     )}
                   />
-                  <Route exact path='/about' component={About} />
                   <Route
                     exact
                     path='/status'
--- services/client/src/components/__tests__/Form.test.jsx
+++ services/client/src/components/__tests__/Form.test.jsx
@@ -8,23 +8,23 @@ import Form from '../Form';
 const testData = [
   {
     formType: 'Register',
+    title: 'Register',
     formData: {
       username: '',
       email: '',
       password: ''
     },
-    handleUserFormSubmit: jest.fn(),
-    handleFormChange: jest.fn(),
+    loginUser: jest.fn(),
     isAuthenticated: false
   },
   {
     formType: 'Login',
+    title: 'Log In',
     formData: {
       email: '',
       password: ''
     },
-    handleUserFormSubmit: jest.fn(),
-    handleFormChange: jest.fn(),
+    loginUser: jest.fn(),
     isAuthenticated: false
   }
 ];
@@ -37,7 +37,7 @@ describe('When not authenticated', () => {
       const wrapper = shallow(component);
       const h1 = wrapper.find('h1');
       expect(h1.length).toBe(1);
-      expect(h1.get(0).props.children).toBe(el.formType);
+      expect(h1.get(0).props.children).toBe(el.title);
       const formGroup = wrapper.find('.field');
       expect(formGroup.get(0).props.children.props.name).toBe(
         Object.keys(el.formData)[0]
@@ -47,14 +47,18 @@ describe('When not authenticated', () => {
 
     it(`${el.formType} Form submits the form properly`, () => {
       const wrapper = shallow(component);
+      wrapper.instance().handleUserFormSubmit = jest.fn();
+      wrapper.update();
       const input = wrapper.find('input[type="email"]');
-      expect(el.handleUserFormSubmit).toHaveBeenCalledTimes(0);
-      expect(el.handleFormChange).toHaveBeenCalledTimes(0);
-      input.simulate('change');
-      expect(el.handleFormChange).toHaveBeenCalledTimes(1);
+      expect(wrapper.instance().handleUserFormSubmit).toHaveBeenCalledTimes(0);
+      input.simulate('change', {
+        target: { name: 'email', value: 'test@test.com' }
+      });
       wrapper.find('form').simulate('submit', el.formData);
-      expect(el.handleUserFormSubmit).toHaveBeenCalledWith(el.formData);
-      expect(el.handleUserFormSubmit).toHaveBeenCalledTimes(1);
+      expect(wrapper.instance().handleUserFormSubmit).toHaveBeenCalledWith(
+        el.formData
+      );
+      expect(wrapper.instance().handleUserFormSubmit).toHaveBeenCalledTimes(1);
     });
 
     it(`${el.formType} Form renders a snapshot properly`, () => {
